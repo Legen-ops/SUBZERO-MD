b7300a6657161dc667e44de892c6fdd39c5c15a0
Legen-ops <videmalk@gmail.com>
2025-10-08 16:15:57 +0100

    Update settings.js for Lkpm

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "MRFRANKX",
+  OWNER_NUMBER: "+237670160178", 
+  PREFIX: ".SUBZERO-MD;;;iEcEECyb#7JnuSR8fcy_0F7_KwBFJSFT65U4PxZ9RML_FgpsIsT0",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
